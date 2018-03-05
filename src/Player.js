8ba26637d9e6c7a3ea19b0db1df83f6d7dadf7d6
Ziad Hamdieh <z.a.hamdieh@gmail.com>
2018-03-05 17:35:11 -0500

    added member variables & functions for player position to the player class

--- src/Player.js
+++ src/Player.js
@@ -8,12 +8,32 @@ class Player {
 			SP:30,
 			Str:9,
 			Dex:9,
+			xPos: 0,
+			yPos: 0
 		}
 		
 		this.expose = this.expose.bind(this);
 		this.saveChar = this.saveChar.bind(this);
 		this.loadChar = this.loadChar.bind(this);
 	}
+
+	getXPos() {
+		return xPos;
+	}
+
+	getYPos() {
+		return yPos;
+	}
+
+	setXPos(x) {
+		xPos += x;
+	}
+
+	setYPos(y) {
+		yPos += y;
+	}
+
+
 	
 	saveChar(){	}
 	loadChar(){	}
