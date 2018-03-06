5575f68f6652a8f6914cbe796325804038edb68f
Ziad Hamdieh <z.a.hamdieh@gmail.com>
2018-03-05 23:20:14 -0500

    added new tile types
    door, staircase, table, chair, and closet.

--- src/Components/Tiles.js
+++ src/Components/Tiles.js
@@ -1,10 +1,50 @@
-var Tiles = [{id:0,
-name:"floor",
-IS_WALKABLE:1},
+var Tiles = [
 
-{id:1,
-name:"wall",
-IS_WALKABLE:0}
+{
+	id:0,
+	name:"floor",
+	IS_WALKABLE:1
+},
+
+{
+	id:1,
+	name:"wall",
+	IS_WALKABLE:0},
+
+{
+	id:2,
+	name:"door",
+	IS_WALKABLE:0,
+	IS_OPEN:0
+
+},
+
+{
+	id:3,
+	name:"staircase",
+	IS_WALKABLE:1
+},
+
+{
+	id:4,
+	name:"table",
+	IS_WALKABLE:0
+},
+
+{
+	id:5,
+	name:"chair",
+	IS_WALKABLE:0
+},
+
+{
+	id:6,
+	name:"closet",
+	IS_WALKABLE:0,
+	IS_OPEN:0
+},
+
 ]
+
 
 export default Tiles;
